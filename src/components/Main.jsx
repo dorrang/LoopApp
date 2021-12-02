76f1fe0d80a2f6a8936e70a1534aa1636baf049b
dorrang <summ.pie@gmail.com>
2021-12-02 16:32:49 +0200

    clean

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,11 +4,6 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Main() {
   return (
-    <Router>
-      <Route path="/">
-        <Drumset2 />
-      </Route>
-
-    </Router>
+    <Drumset2 />
   );
 }
