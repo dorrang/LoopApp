4e9a531d173b062581c9b813b04f330f2cb945af
dorrang <summ.pie@gmail.com>
2021-12-02 16:12:38 +0200

    clean

--- src/components/Drumpad2.jsx
+++ src/components/Drumpad2.jsx
@@ -2,47 +2,6 @@ import React, { useEffect, useState } from "react";
 
 export default function Drumpad(props) {
 
-
-
-
-  const [audio] = useState(new Audio(props.pad.soundURL))
-
-
-  // useEffect(() => {
-  //   console.log(props.pad);
-  //   if (props.pad.isActive) {
-  //     props.toggleActive(props.pad.id)
-  //     audio.muted = false;
-  //     audio.loop = true;
-  //     console.log(audio.currentTime)
-
-  /// FINDING THE ACTIVE PADS, MAPPING TO AN ARRAY
-  // let activePads = props.pads.filter((pad) => {
-  //   console.log(pad)
-  //   if (pad.isActive) {
-  //     console.log(pad.id)
-  //     return true
-  //   }
-  // }).map((pad) => pad.id);
-
-  // console.log('active pads ', activePads);
-  // let cache = setInterval(() => { audio.play() }
-  //   , 2000 - audio.currentTime)
-
-  // audio.play()
-  //   } else {
-  //     audio.muted = true;
-  //     props.toggleActive(props.pad.id);
-  //     // audio.pause()
-  //     audio.currentTime = 0;
-  //   }
-  // }, [props.pad.isActive])
-
-
-
-
-
-
   return (
     <div className="inline">
 
