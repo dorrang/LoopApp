ddcd62a8fe83a7fc2c19522d10277ac6e474c6c9
dorrang <summ.pie@gmail.com>
2021-12-02 17:23:47 +0200

    Updates

---
+++ precache-manifest.4d33426d027b9e53057bf2d84d6e0242.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7882b180948848303756334c9858e1",
+    "url": "/LoopApp/index.html"
+  },
+  {
+    "revision": "ab58f40469cb2ac53676",
+    "url": "/LoopApp/static/css/2.3ac2345a.chunk.css"
+  },
+  {
+    "revision": "ab58f40469cb2ac53676",
+    "url": "/LoopApp/static/js/2.ee456a92.chunk.js"
+  },
+  {
+    "revision": "eb60a1ddee9bdaf329d6c80899bb62b7",
+    "url": "/LoopApp/static/js/2.ee456a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8f02aedaa787c0975e",
+    "url": "/LoopApp/static/js/main.ee68e852.chunk.js"
+  },
+  {
+    "revision": "d25cfc26226277e23e71",
+    "url": "/LoopApp/static/js/runtime-main.f4383301.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/LoopApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/LoopApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/LoopApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/LoopApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/LoopApp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
